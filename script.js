cdf4fc197c739f45a915ca77fb109787ea1709aa
Altair Penson <36816323+diazdressk@users.noreply.github.com>
2021-02-28 13:09:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const panels = document.querySelectorAll('.panel') //выбираю все панели по селектору класс panel и помещаю их в массив panels
-
+const panels = document.querySelectorAll('.panel') 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
-    removeActiveClasses();//вызываю функцию, кот удаляет класс activ
-    panel.classList.add('active');//добавляю класс актив при нажатии на див, panel становится panel active, к которому применен отлдельный стиль в сss
+    removeActiveClasses();
+    panel.classList.add('active');
   })
 });
 
 function removeActiveClasses() {
-  panels.forEach(panel => {//в стрелочной функции,если передается только один аргумент,могу скобки не ставить
+  panels.forEach(panel => {
     panel.classList.remove('active');
   })
 };
